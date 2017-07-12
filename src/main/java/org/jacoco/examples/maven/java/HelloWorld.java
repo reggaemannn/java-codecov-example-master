a8ac72a279fdc5e13cf3ec5311f6a663db1d0b33
reggaemannn <reggaemannn@gmail.com>
2017-07-12 13:16:55 +0200

    initial commit

---
+++ src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
@@ -0,0 +1,33 @@
+package org.jacoco.examples.maven.java;
+
+import java.util.Random;
+
+public class HelloWorld {
+	
+	public String getMessage(boolean bigger) {
+		if (bigger) {
+			return "Hello Universe!";
+		} else {
+			return "Hello World!";
+		}
+	}
+
+	public String getMsg(boolean bigger) {
+		if (bigger&&0<1) {
+			return "Hello Universe!";
+		} else {
+			return "Hello World!";
+		}
+	}
+
+    public String getMsg2(boolean bigger) {
+        if (bigger&&0<1) {
+            return "Hello Universe!";
+        } else {
+            return "Hello World!";
+        }
+
+        if (bigger&&0<1)
+            return "Hello World!";
+    }
+}
